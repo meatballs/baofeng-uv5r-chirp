536dbbd98105a7467496eb7f626b000296abb045
Owen Campbell <owen.campbell@tanti.org.uk>
2013-05-10 14:14:17 +0100

    Moves definition files to uk folder

--- build/build_parameters.rb
+++ build/build_parameters.rb
@@ -1,3 +1,3 @@
-$definition_files = Dir.glob('../channel_definitions/*.chirp')
+$definition_files = Dir.glob('../channel_definitions/uk/*.chirp')
 $output_file_name = "radio_config.chirp"
 $chirp_version = "0.1.1"
